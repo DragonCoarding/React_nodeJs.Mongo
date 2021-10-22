af4d55409206322f181255509b57a6d408d43bc1
DragonCoarding <dydvkdvkd1@naver.com>
2021-10-23 02:45:05 +0900

    success login OMG ~

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const config = require('./config/key');
 const {User} = require("./model/User");
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,21 +12,55 @@ app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err))
 
 
-app.get('/',(req,res) => res.send('히히히 자바 '))
+app.get('/',(req,res) => res.send('히히히 자바'))
 
 app.post('/register', (req,res) => {
     // 회원가입에 필요한 정보들을 client에서 가져오면 해당정보들은 DB에 넣어준다.
     const user = new User(req.body)
 
+    // 모델에 세이브하기전 암호화를 먼저 시켜줘야한다 (모델에 userSchma.pre('save') 를 쓰면 save하기전에 뭔가를 해라 라고 지시함 )
+
     user.save((err,userInfo) => {
         if(err) return res.json({ success: false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req,res) => {
+    
+    // 요청된 이메일을 데이터베이스에서 있는지 찾아본다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없당"
+            })       
+        }
+
+
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+
+        user.comparePassword(req.body.password, (err, isMatch ) => {
+            if(!isMatch)
+            return res.json({ loginSuccess: false, message: "비밀번호틀림~"  })
+        
+            user.makeToken((err , user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에 ???? 쿠키? 로컬스토리지 ? 어디가 가장안전한가에 대한건 논란이있당 .
+                res.cookie("x_auth", user.token) 
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+           })
         })
     })
 })
--- model/User.js
+++ model/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
 const userSchma = mongoose.Schema({
     name:{
         type: String,
@@ -25,6 +28,52 @@ const userSchma = mongoose.Schema({
         type: Number
     }
 })
+
+userSchma.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password')) {
+        //비밀번호를 암호화시키고 index의 save 함수로 보냄
+        bcrypt.genSalt(saltRounds,function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password,salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    } 
+})
+
+
+userSchma.methods.comparePassword = function(plainPassword, cb){
+
+    // plainPassword 12341234 , 암호화된 비밀번호 $2b$10$ZCmr2Hrk02OnMbhpCV2KvuTpEG7jhGeaewkyEbrPjHWlO/SSvlA2G
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchma.methods.makeToken = function(cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 token을 생성하깅!
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+
+    // user._id + 'secretToken' = token
+
+    // ->
+
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchma) // 모델로 감싸주기  모델이름 , 스키마이름
 
