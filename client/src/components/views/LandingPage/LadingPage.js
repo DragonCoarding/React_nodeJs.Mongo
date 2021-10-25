52ae3d79c8d2dec5c7df01659db27c099f68b743
DragonCoarding <dydvkdvkd1@naver.com>
2021-10-25 14:40:44 +0900

    33 Logout ㅠ.ㅠ

--- client/src/components/views/LandingPage/LadingPage.js
+++ client/src/components/views/LandingPage/LadingPage.js
@@ -1,22 +1,31 @@
-import React, {useEffect} from 'react'
-import axios from  'axios'
+import React from 'react';
+import axios from 'axios';
 
+function LadingPage(props) {
 
-
-
-function LadingPage() {
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response))
-    }, [])
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(response => {
+           if(response.data){
+                props.history.push("/login")
+           } else { 
+                alert('실패했다 썩 돌아가!')
+           }  
+        })
+    }   
+            
+ 
 
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
-        
         }}>
             <h2>HOME</h2>
+
+        <button onClick={onClickHandler}>
+            로그아웃
+        </button>
+
         </div>
     )
 }
